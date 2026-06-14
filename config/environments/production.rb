2f35bcaee6ff4624c059712ec841fb3244604fba
borz185 <vladimir.borz.185@yandex.ru>
2026-06-14 08:31:06 +0300

    Deploy Render12

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << "bookingapp.site"
+  config.hosts << "www.bookingapp.site"
 end
